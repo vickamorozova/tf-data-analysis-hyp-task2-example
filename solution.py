be1550a559be01a1650866510031729a4db02536
Viktoriya Morozova <117348526+vickamorozova@users.noreply.github.com>
2023-04-12 17:59:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 357282961 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return anderson_ksamp([x, y]).pvalue < 0.09
+def solution(x_success: int, 
+             x_cnt: int, 
+             y_success: int, 
+             y_cnt: int) -> bool:
+    
+    _, pvalue = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+
+    # выявляем наличие эффекта
+    effect = (pvalue < 0.08)
+    
+    return effect
