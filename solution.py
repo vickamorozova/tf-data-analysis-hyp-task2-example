bb15bd47f1336ab465ba4f58c7fbe80f8b8c22cd
Viktoriya Morozova <117348526+vickamorozova@users.noreply.github.com>
2023-04-12 08:20:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,4 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return anderson_ksamp([x, y]).pvalue < 0.05
+    return anderson_ksamp([x, y]).pvalue < 0.09
